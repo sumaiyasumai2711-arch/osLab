f0401884457b7dcf619df43b80cbcae6a59f8d7b
sumaiyasumai2711-arch <sumaiyasumai2711@gmail.com>
2026-09-16 15:21:39 +0530

    Delete osLab/program11.sh

--- osLab/program11.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "=====Systemm information ====="
-echo "Hostname: $(hostname)"
-echo "User   :$(whoami)"
-echo "Kernel :$(uname -r)"
-echo "Date   :$(date)"
-echo "Uptime :$(uptime)"
